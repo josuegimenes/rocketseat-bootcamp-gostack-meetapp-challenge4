bdedd7ad0dbe0feb346d8a99c0704d6e0d70f869
Josué Gimenes <josuegalvares@gmail.com>
2019-09-26 09:52:14 -0300

    Setting style-loader and css-loader for CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 // Importar o React toda vez que possuir sintaxe html no arquivo.
 import React from 'react';
 
+// Importando e aplicando o CSS.
+import './App.css'
+
 function App() {
   return <h1>Hello Facebook</h1>
 }
